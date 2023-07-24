c9b2db2276427b5c0f316e32e73732342a6fcb2d
selim <selimouadi31@gmail.com>
2023-07-24 17:08:38 +0200

    initialisation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Layout/Navbar'
+import Home from './pages/Home'
 
 function App() {
 
   return (
-    <>
-    </>
+    <Router>
+      <Navbar/>
+        <Routes>
+        <Route path="/" element ={<Home/>} />
+        </Routes>
+    </Router>
   )
 }
 
