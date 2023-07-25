ffb4158c5aff9a637a256d0f2b934234a88f6615
dorance <sarah.dorance@live.fr>
2023-07-25 11:37:01 +0200

    commit sarah

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className='cards'>Home
+      <CardRecipe image="" title="titre" des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, sequi molestiae! Dignissimos magni architecto nesciunt cupiditate odio quae ducimus perspiciatis sapiente est. Soluta totam quo dolore dolores enim laboriosam perspiciatis!"/>
+      <CardRecipe image="" title="titre" des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, sequi molestiae! Dignissimos magni architecto nesciunt cupiditate odio quae ducimus perspiciatis sapiente est. Soluta totam quo dolore dolores enim laboriosam perspiciatis!"/>
+
     </div>
   )
 }
