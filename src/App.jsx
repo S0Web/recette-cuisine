429be0fe13f3f950a0410ebac97f0ed685b8903c
selim <selimouadi31@gmail.com>
2023-07-25 10:59:13 +0200

    Navbar and browserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.css'
 import Navbar from './components/Layout/Navbar'
 import Home from './pages/Home'
 import Entrees from './pages/Entrees'
 import Plats from './pages/Plats'
 import Desserts from './pages/Desserts'
+import AddRecipe from './pages/AddRecipe'
 
 
 function App() {
 
   return (
+    <>
     <BrowserRouter>
       <Navbar/>
         <Routes>
-        <Route path="/home" element={<Home/>} />
+        <Route path="/" element={<Home/>} />
         <Route path="/entrees" element={<Entrees/>} />
         <Route path="/plats" element={<Plats/>} />
         <Route path="/desserts" element={<Desserts/>} />
+        <Route path="/addrecipe" element={<AddRecipe/>} />
         </Routes>
-    </BrowserRouter>
+      </BrowserRouter>
+    </>
   )
 }
 
--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './Navbar.css'
 
 const Navbar = () => {
   return (
-    <div>
-      <Link to="/home">Home</Link>
-      <Link to="/entrees">Entrées</Link>
-      <Link to="/plats">Plats</Link>
-      <Link to="/desserts">Desserts</Link>
-              
+    <div className="navbar-content">
+      <h1>
+        <Link to="/">SPS's Recipe</Link>
+      </h1>
+
+      <div className="navbar-container">    
+        <Link to="/entrees">Entrées</Link>
+        <Link to="/plats">Plats</Link>
+        <Link to="/desserts">Desserts</Link>
+        <Link to="/AddRecipe">Créer une recette</Link>
+      </div>
     </div>
   )
 }
