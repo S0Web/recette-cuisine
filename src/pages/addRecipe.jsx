51aeaaad4360f0d04f6d0c72a8feb78c2c5ca195
ynwbape <peezopro@gmail.com>
2023-07-25 15:45:31 +0200

    add

---
+++ src/pages/addRecipe.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import RecipeForm from '../components/addRecipe/RecipeForm'
+
+import "./AddRecipe.css"
+const AddRecipe = () => {
+  return (
+    <div className='addrecipe-page'>
+      <RecipeForm />
+    </div>
+  )
+}
+
+export default AddRecipe
