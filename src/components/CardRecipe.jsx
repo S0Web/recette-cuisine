b8235f6b1126acc1e3bb47d4e7f71e1b758a51cf
dorance <sarah.dorance@live.fr>
2023-07-25 15:06:49 +0200

    commit sarah ca

--- src/components/CardRecipe.jsx
+++ src/components/CardRecipe.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import "./CardRecipe.css"
 
 
-const CardRecipe = ({image, title, dif, des}) => {
-    const im = {image}
+const CardRecipe = ({image, title, dif, temps, des}) => {
   return (
     <div className="Ca">
-        <img src="" alt="image recette" width={50} height={50} />
+        <img src={image} alt="image recette" width={50} height={50} />
         <h3>{title}</h3>
         <p>{des}</p>
-        <p>{dif}</p>
+        <p>Difficulté : {dif}</p>
+        <p>Temps de préparation : {temps}</p>
     </div>
   )
 }
