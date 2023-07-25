81735add8643068e7b29976f7abcdcccc907de3d
selim <selimouadi31@gmail.com>
2023-07-25 15:46:52 +0200

    on remet AddRecipe

---
+++ src/pages/addRecipe.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import RecipeForm from '../components/addRecipe/RecipeForm'
+
+import "./AddRecipe.css"
+//refssdf
+const AddRecipe = () => {
+  return (
+    <div className='addrecipe-page'>
+      <RecipeForm />
+    </div>
+  )
+}
+
+export default AddRecipe
